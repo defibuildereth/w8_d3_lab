55cef28929b67838f6635ec038c8469c0eda1874
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-08 11:12:18 +0100

    can create

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -7,15 +7,29 @@ const createRouter = function (collection) {
 
     router.get('/', (req, res) => {
         collection
-        .find()
-        .toArray()
-        .then((docs) => res.json(docs))
-        .catch((err) => {
-            console.error(err)
-            res.status(500)
-            res.json({status:500, error:err})
-        })
+            .find()
+            .toArray()
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({ status: 500, error: err })
+            })
     })
+
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection
+            .insertOne(newData)
+            .then((result) => {
+                res.json(result)
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
+            });
+    });
 
     return router;
 };
